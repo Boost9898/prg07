a2c32b6827fa02766a40192fd4cf32706ce1d17c
Boost9898 <bls.place@gmail.com>
2021-07-01 21:28:36 +0200

    added comments

--- Eindopdracht/ProfitChecker/app/src/main/java/com/example/profitchecker/MainActivity.java
+++ Eindopdracht/ProfitChecker/app/src/main/java/com/example/profitchecker/MainActivity.java
@@ -38,19 +38,27 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // const with log tag for debugging, const uri which is filled with an API
+    // const with log tag for debugging, const uri which is filled with an API, request code and location client
     private final static String LOG_TAG = "ProfitChecker";
     private final static String URI = "https://hiveon.net/api/v1/stats/pool";
     private final static int REQ_CODE_LAST_KNOWN_LOCATION = 1234567890;
     private FusedLocationProviderClient fusedLocationClient;
 
-    // costs with exchange rate from USD to other currency (i know this isn't the best way to do it but it works)
+    // const with exchange rate from USD to other currency (i know this isn't the best way to do it but it works)
+    private final static double EXCHANGE_USD_USD = 1.00;
     private final static double EXCHANGE_USD_EUR = 0.840667;
     private final static double EXCHANGE_USD_GBP = 0.722478;
     private final static double EXCHANGE_USD_JPY = 110.471513;
-    private final static double EXCHANGE_USD_INR = 74.379222;
 
-    // variables to save data from api
+    // variable with default currency
+    public static String currency = "$";
+    public static String countryName = "";
+
+    // variables to store GPS coordinates
+    public double latitude = 0;
+    public double longitude = 0;
+
+    // variables to store data from api
     public double exchangeRatesUsd = 0;
     public double expectedReward24H = 0;
     public double expectedProfitabilityUSD = 0;
@@ -59,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     LocationRequest locationRequest;
 
 
-    @Override
+    @Override // default start function
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -67,26 +75,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = findViewById(R.id.btn_settings);
         button.setOnClickListener(this);
 
-//        getProfitability();
-
         // get latest known location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         showCurrentLocation();
+        getProfitability();
     }
 
 
-    @Override
+    @Override // opens settings activity
     public void onClick(View view) {
-        // opens settings activity
-        Log.d(LOG_TAG, "Click");
+        // create intent and link it to the new activity and start it
         Intent i = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(i);
     }
 
-    @Override
+
+    @Override // when a user comes back to the app, it executes this function
     protected void onResume() {
         // reads current status of languageSwitch (settings.xml) and logs it
-        // calls getProfitability again when coming back to this screen to update to the latest information
+        // calls getProfitability/showCurrentLocation again to update to the latest information
         Log.d(LOG_TAG, "Language switch: " + SettingsActivity.languageSwitch(this));
         super.onResume();
         getProfitability();
@@ -95,7 +102,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void getProfitability() {
-        // instantiate the RequestQueue
+        // instantiate the RequestQueue (internet)
+        // using Volley to handle the internet connection
         RequestQueue queue = Volley.newRequestQueue(this);
 
         // request a string response from the provided URL
@@ -103,11 +111,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Response.Listener<JSONObject>() {
                     @Override // send log on response
                     public void onResponse(JSONObject response) {
-                        Log.d(LOG_TAG, response.toString());
+//                        Log.d(LOG_TAG, response.toString());
                         showProfitability(response);
                     }
                 }, new Response.ErrorListener() {
-            @Override
+            @Override // logs funny error
             public void onErrorResponse(VolleyError error) {
                 Log.e(LOG_TAG, "[TASK FAILED SUCCESSFULLY] " + error);
             }
@@ -118,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n") // complex method which gets data out of api + converts GPS coordinates to country + updates textview
     private void showProfitability(JSONObject data) {
         try { // get request on api 'stats' and dive deeper in child objects
             JSONObject stats = (JSONObject) data.get("stats");
@@ -134,6 +142,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // multiple exchangeRatesUsd by expectedReward24H to calculate the current profitability per 100 MH/s in USD and store it in a global variable
             expectedProfitabilityUSD = exchangeRatesUsd * expectedReward24H;
 
+            // uses Geocoder to convert lat/long coordinates to country
+            Context context = getApplicationContext();
+            Geocoder gcd = new Geocoder(context, Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = gcd.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (addresses.size() > 0)
+            {
+                countryName = addresses.get(0).getCountryName();
+                Log.d(LOG_TAG,  "Current country: " + countryName);
+
+            } else {
+                Log.d(LOG_TAG, "No country found");
+            }
+
+
+            // update current eth profit per 10000 mhs to textview and add ether sign in front of it
+            // added math.round 100000 to round it to 5 decimals
+            TextView tv_expectedReward24H = findViewById(R.id.txt_profitValue100ether);
+            tv_expectedReward24H.setText("Ξ " + Math.round(expectedReward24H * 100000) / 100000.0);
+
+            // check which country you're in and update currency symbol + apply exchange rate + round decimals to 2 digits
+            TextView tv_exchangeRatesUsd;
+            TextView tv_expectedProfitabilityUSD;
+            switch(countryName) {
+                case "Germany":
+                case "Netherlands":
+                    Log.d(LOG_TAG, "Dit is Germany, Netherlands");
+                    currency = "€";
+                    // update current eth price to textview and add currency symbol in front of it
+                    tv_exchangeRatesUsd = findViewById(R.id.txt_exchangeRatesUsd);
+                    tv_exchangeRatesUsd.setText(currency + " " + (Math.round(exchangeRatesUsd * EXCHANGE_USD_EUR * 100) / 100.0));
+                    // update current usd profit per 100 mhs to textview and add dollar sign in front of it
+                    // added math.round 100 to round it to 2 decimals
+                    tv_expectedProfitabilityUSD = findViewById(R.id.txt_profitValue100usd);
+                    tv_expectedProfitabilityUSD.setText(currency + " " + Math.round(expectedProfitabilityUSD * 100 * EXCHANGE_USD_EUR) / 100.0);
+                    break;
+                case "United Kingdom":
+                    Log.d(LOG_TAG, "Dit is United Kingdom");
+                    currency = "£";
+                    tv_exchangeRatesUsd = findViewById(R.id.txt_exchangeRatesUsd);
+                    tv_exchangeRatesUsd.setText(currency + " " + (Math.round(exchangeRatesUsd * EXCHANGE_USD_GBP * 100) / 100.0));
+                    tv_expectedProfitabilityUSD = findViewById(R.id.txt_profitValue100usd);
+                    tv_expectedProfitabilityUSD.setText(currency + " " + Math.round(expectedProfitabilityUSD * 100 * EXCHANGE_USD_GBP) / 100.0);
+                    break;
+                case "Japan":
+                    Log.d(LOG_TAG, "Dit is Japan");
+                    currency = "¥";
+                    tv_exchangeRatesUsd = findViewById(R.id.txt_exchangeRatesUsd);
+                    tv_exchangeRatesUsd.setText(currency + " " + (Math.round(exchangeRatesUsd * EXCHANGE_USD_JPY * 100) / 100.0));
+                    tv_expectedProfitabilityUSD = findViewById(R.id.txt_profitValue100usd);
+                    tv_expectedProfitabilityUSD.setText(currency + " " + Math.round(expectedProfitabilityUSD * 100 * EXCHANGE_USD_JPY) / 100.0);
+                    break;
+                default:
+                    Log.d(LOG_TAG, "Dit is default (Murica)");
+                    currency = "$";
+                    tv_exchangeRatesUsd = findViewById(R.id.txt_exchangeRatesUsd);
+                    tv_exchangeRatesUsd.setText(currency + " " + (Math.round(exchangeRatesUsd * EXCHANGE_USD_USD * 100) / 100.0));
+                    tv_expectedProfitabilityUSD = findViewById(R.id.txt_profitValue100usd);
+                    tv_expectedProfitabilityUSD.setText(currency + " " + Math.round(expectedProfitabilityUSD * 100 * EXCHANGE_USD_USD) / 100.0);
+            }
+
         } catch (JSONException e) {
             Log.e(LOG_TAG, "[TASK FAILED SUCCESSFULLY] JSON exception");
         }
@@ -142,21 +216,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d(LOG_TAG, String.valueOf(exchangeRatesUsd));
         Log.d(LOG_TAG, String.valueOf(expectedReward24H));
         Log.d(LOG_TAG, String.valueOf(expectedProfitabilityUSD));
-
-        // update current eth price to textview and add dollar sign in front of it
-        TextView tv_exchangeRatesUsd = findViewById(R.id.txt_exchangeRatesUsd);
-        tv_exchangeRatesUsd.setText("$ " + exchangeRatesUsd);
-
-        // update current usd profit per 100 mhs to textview and add dollar sign in front of it
-        // added math.round 100 to round it to 2 decimals
-        TextView tv_expectedProfitabilityUSD = findViewById(R.id.txt_profitValue100usd);
-        tv_expectedProfitabilityUSD.setText("$ " + Math.round(expectedProfitabilityUSD * 100) / 100.0);
-
-        // update current eth profit per 10000 mhs to textview and add ether sign in front of it
-        // added math.round 100000 to round it to 5 decimals
-        TextView tv_expectedReward24H = findViewById(R.id.txt_profitValue100ether);
-        tv_expectedReward24H.setText("Ξ " + Math.round(expectedReward24H * 100000) / 100000.0);
     }
+
 
     private void showCurrentLocation() {
         Log.d(LOG_TAG, "showCurrentLocation() is aangeroepen");
@@ -168,6 +229,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
@@ -175,23 +237,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Log.d(LOG_TAG, "Last Location Latitude: " + location.getLatitude());
                             Log.d(LOG_TAG, "Last Location Longitude: " + location.getLongitude());
 
-                            double latitude = location.getLatitude();
-                            double longitude = location.getLongitude();
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
 
-                            Context context = getApplicationContext();
-                            Geocoder gcd = new Geocoder(context, Locale.getDefault());
-                            List<Address> addresses = null;
-                            try {
-                                addresses = gcd.getFromLocation(latitude, longitude, 1);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                            if (addresses.size() > 0)
-                            {
-                                String countryName=addresses.get(0).getCountryName();
-                                Log.d(LOG_TAG, "" + countryName);
-                            }
+                            getProfitability();
 
                         } else {
                             Log.d(LOG_TAG, "No last know location available :(");
@@ -211,7 +260,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     showCurrentLocation();
                 } else {
                     Log.d(LOG_TAG, "User denied permissions");
-                    easterEgg();
                 }
                 return;
         }
